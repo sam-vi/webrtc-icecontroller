5eb0f7c6b4992e90f72aec813c30e52f963ec617
Sameer Vijaykar <194338+sam-vi@users.noreply.github.com>
2022-11-30 15:03:32 +0100

    Bootstrap the spec

---
+++ respec-config.js
@@ -0,0 +1,34 @@
+var respecConfig = {
+  specStatus: "unofficial",
+  // if there a publicly available Editor's Draft, this is the link
+  // edDraftURI: "https://w3c.github.io/webrtc-icecontroller/",
+  shortName: "webrtc-icecontroller",
+  editors: [
+    { name: "Sameer Vijaykar", company: "Google", w3cid: "00000" },
+    { name: "Harald Alvestrand", company: "Google", w3cid: "00000" } 
+  ],
+  authors: [
+  ],
+  group: "webrtc",
+  wgPublicList: "public-webrtc",
+  github: "https://github.com/sam-vi/webrtc-icecontroller",
+  // testSuiteURI: "https://github.com/web-platform-tests/wpt/tree/master/webrtc-icecontroller/",
+  otherLinks: [
+    {
+      key: "Participate",
+      data: [
+        {
+          value: "Mailing list",
+          href: "https://lists.w3.org/Archives/Public/public-webrtc/"
+        },
+        {
+          value: "IETF ICE Working Group",
+          href: "https://datatracker.ietf.org/wg/ice/"
+        }
+      ]
+    }
+  ],
+  xref: ["webrtc", "html", "webidl"],
+  localBiblio: {
+  }
+}
